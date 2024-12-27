38e0c53f1183b45ce0cdecb2fa7991d1612c9720
qBlackbirdp <blackbird9711@gmail.com>
2024-12-27 15:13:18 +0900

    디렉토리 생성 및 구글 로그인 버튼 생성:

--- src/components/member/GoogleLoginButton.js
+++ src/components/member/GoogleLoginButton.js
@@ -1,2 +1,31 @@
 // GoogleLoginButton.js
 
+import React from "react";
+
+const GoogleLoginButton = () => {
+    const googleLogin = () => {
+        console.log("%c[INFO] Google 로그인 시도 중...", "color: blue");
+        const backendUrl =
+            process.env.REACT_APP_BACKEND_URL || "localhost:8030";
+        console.log(process.env.REACT_APP_BACKEND_URL);
+        window.location.href = `${backendUrl}/oauth2/authorization/google`; // Google OAuth2 로그인 경로
+    };
+
+    return (
+        <button
+            onClick={googleLogin}
+            className="google-login-button"
+        >
+            <div className="bc-logo">
+                <img
+                    src="https://developers.google.com/identity/images/g-logo.png"
+                    className="google-logo"
+                    alt="Google logo"
+                />
+            </div>
+            <span className="login-text">Sign in with Google</span>
+        </button>
+    );
+};
+
+export default GoogleLoginButton;
